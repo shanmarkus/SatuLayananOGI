f3050b0e578324fc3af535ea31b78cf1c8c1e4f0
shanmarkus <shanmarkus@gmail.com>
2014-01-19 16:23:08 +0700

    fixing Tentang.js view :3

--- Resources/tentang.js
+++ Resources/tentang.js
@@ -3,18 +3,29 @@
  */
 var win = Titanium.UI.currentWindow;
 
-Ti.App.addEventListener('openURL', function(e){
-    Ti.Platform.openURL(e.url);
+Ti.App.addEventListener('openURL', function(e) {
+	Ti.Platform.openURL(e.url);
 });
 Ti.App.addEventListener('sendEmail', function(e) {
 
-var emailDialog = Titanium.UI.createEmailDialog();
-emailDialog.toRecipients = [e.url];
-emailDialog.open();
-   });
-   
+	var emailDialog = Titanium.UI.createEmailDialog();
+	emailDialog.toRecipients = [e.url];
+	emailDialog.open();
+});
+
 var tentang_webview = Ti.UI.createWebView({
-    url:'tentang.html'
+	url : 'tentang.html',
+	
+});
+
+var categoriesTable = Titanium.UI.createTableView({
+    height: Ti.Platform.displayCaps.platformHeight - 100,
+    width:  320,
+    top:    0,
+    left:   0,
+    
 }); 
+
+categoriesTable.add(tentang_webview);
 // tentang_webview.height='auto';
-win.add(tentang_webview);
+win.add(categoriesTable);
