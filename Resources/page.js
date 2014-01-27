6cb43b34c363e0e5f26cf6e01a6c2feafb4a5d57
shanmarkus <shanmarkus@gmail.com>
2014-01-27 16:36:32 +0700

    adding disqus to the view
    still confuse how to add the stuff. Because bentrok kalo gw add win.add
    web view true gw add lagi dengan disqus view itu ga may keluar disqus
    view nya

--- Resources/page.js
+++ Resources/page.js
@@ -21,19 +21,12 @@ ContentHTTPClient.onload = function() {
 
 	var jsonObject = JSON.parse(this.responseText);
 
-    //generating json content to web view
-    var webview= Ti.UI.createWebView({
-        html: "<style>body{font-family:'HelveticaNeue-Light';font-size:16px}"+
-        "img{max-width:300px;} "+
-        ".content{padding:5px}"+
-        "td, th{ width: 4rem;height: 2rem;border: 1px solid #ccc;text-align: center;}"+
-        "th {background: lightblue;border-color: white;}"+
-        "body {padding: 1rem;}</style><div class='content'>"+jsonObject.content[0].content+"</div>"
-    });
-
-	var disqusView = Ti.UI.createWebView({
-	url: "http://satulayanan.net/comment/index/"+moduleSlug+"/"+pageSlug+"",
-	
+	//generating json content to web view
+	var webview = Ti.UI.createWebView({
+		html : "<head><meta name=\"viewport\" content=\"width=device-width, user-scalable=no\"></head><style>body{font-family:'HelveticaNeue-Light';font-size:16px}" + "img{max-width:300px;} " + ".content{padding:5px}" + "td, th{ width: 4rem;height: 2rem;border: 1px solid #ccc;text-align: center;}" + "th {background: lightblue;border-color: white;}" + "body {padding: 1rem;}</style><div class='content'>" + jsonObject.content[0].content + "</div>",
+		top:5,
+		height:260,
+		
 	});
 
 	//title label for row at index i
@@ -50,11 +43,20 @@ ContentHTTPClient.onload = function() {
 		height : Ti.Platform.displayCaps.platformHeight,
 		color : '#232'
 	});
-	//add web view to window
 	win.add(webview);
-	win.add(disqusView);
+
 };
 
+//add web view to window
+
+var disqusView = Ti.UI.createWebView({
+	url : "http://satulayanan.net/comment/index/" + moduleSlug + "/" + pageSlug + "",
+	scalesPageToFit : true,
+	top : 280,
+});
+//win.add(disqusView);
+
+win.add(disqusView);
 //this method will fire if there's an error in accessing the //remote data
 ContentHTTPClient.onerror = function() {
 	// log the error to our Ti tanium Studio console
